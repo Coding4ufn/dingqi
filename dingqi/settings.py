45fc971799fcaf15761b59e51b3e0b07dc750047
Tianqi <tongtianqi@kbstreet.com>
2016-05-10 22:53:09 +0800

    bug fixes

--- dingqi/settings.py
+++ dingqi/settings.py
@@ -106,6 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/eventmay/templates',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
@@ -123,8 +132,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-
 
 from wechat.settings import *
