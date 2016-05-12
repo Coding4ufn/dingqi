38db0a3c261356c2c4e283d915ae0da94d9b82c3
Tianqi <tongtianqi@kbstreet.com>
2016-05-13 00:57:06 +0800

    update settings

--- dingqi/settings.py
+++ dingqi/settings.py
@@ -16,6 +16,9 @@ from server_account_config import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+WEB_SITE_ROOT = 'http://www.52dingqi.com'
+
+MOBILE_SITE = 'www.52dingqi.com'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
