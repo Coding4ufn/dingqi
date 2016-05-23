cba7eee4120271635d86547ff0218f8fefa3d835
Tianqi <tongtianqi@kbstreet.com>
2016-05-23 14:17:21 +0800

    bug fixes

--- eventmay/models.py
+++ eventmay/models.py
@@ -9,6 +9,6 @@ from utils import get_score
 
 class Help(models.Model):
     created = models.DateTimeField(u'帮助时间', auto_now_add=True)
-    helper = models.ForeignKey(WechatUser, related_name='help_r')
-    helped = models.ForeignKey(WechatUser, related_name='help_r')
+    helper = models.ForeignKey(WechatUser, related_name='helped')
+    helped = models.ForeignKey(WechatUser, related_name='helped_by')
     score = models.IntegerField(u'帮助数值', default=get_score)
