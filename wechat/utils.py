625e9851e3f21dd7f7b58d5ace30992e12484079
Tianqi <tongtianqi@kbstreet.com>
2016-05-23 15:17:36 +0800

    bug fixes

--- wechat/utils.py
+++ wechat/utils.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.utils import timezone
 from django.utils.encoding import smart_str
 from wechat.wechatauth import WechatMPAuth
-from wechat.models import WechatSettings
 import random
 import urlparse
 import hashlib
@@ -45,42 +44,3 @@ def wx_unescape_url(string):
     for k, v in replace_list.iteritems():
         string = string.replace(k, v)
     return string
-
-
-def wx_get_read_num_by_url(url):
-    """
-    公众文章阅读数读取
-    url : 微信公众文章url地址
-    返回:
-    阅读数, 点赞数
-    """
-    uin = WechatSettings.objects.get(key='wechat_uin').value
-    key = WechatSettings.objects.get(key='wechat_key').value
-    result = urlparse.urlparse(url)
-    res_values = urlparse.parse_qs(result.query, True)
-    biz = res_values['__biz'][0]
-    mid = res_values['mid'][0]
-    sn = res_values['sn'][0]
-    idx = res_values['idx'][0]
-    base_url = 'http://mp.weixin.qq.com/mp/getappmsgext'
-    para = {
-        'key': key,
-        '__biz': biz,
-        'mid': mid,
-        'sn': sn,
-        'idx': idx,
-        'uin': uin,
-    }
-    headers = {
-        'User-Agent': 'MQQBrowser/5.4 TBS/025483 Mobile Safari/533.1 MicroMessenger/6.3.8.56_re6b2553.680 NetType/WIFI Language/zh_CN'
-    }
-    res = requests.post(base_url, headers=headers, params=para, data={'is_only_read': 1})
-    print res.json()
-    try:
-        read = res.json()['appmsgstat']['real_read_num']
-        if read == 0:
-            read = res.json()['appmsgstat']['read_num']
-        like = res.json()['appmsgstat']['like_num']
-    except KeyError:
-        raise Exception(u'read_num empty %s' %  res.json())
-    return read, like
