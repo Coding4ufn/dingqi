c1a81e62e96b5b156550910a0523c4e0c64cd334
Tianqi <tongtianqi@kbstreet.com>
2016-05-23 15:19:16 +0800

    bug fixes

--- wechat/models.py
+++ wechat/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from utils import get_score
+from eventmay.utils import get_score
 
 
 class WechatSettings(models.Model):
